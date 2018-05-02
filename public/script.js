2f4319cebbb0b9fe1740ed26bf3484328fd7b28d
chunktooth <om.saetehn@gmail.com>
2018-05-01 18:59:48 -0600

    locked color palette and working on setting up new palette with server

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 $(document).ready(displayColors);
-$('.brush-btn').on('click', displayColors);
+$('.generate-btn').on('click', displayColors);
 $('.locker').on('click', toggleImage);
+$('.save-project').on('click', saveProject);
+$('.save-palette').on('click', savePalette);
 
 const palettes = [
   $('.palette1'),
@@ -10,36 +12,56 @@ const palettes = [
   $('.palette5')
 ];
 
+var hexArray = [];
+
 function generateHex() {
   var digits = '0123456789ABCDEF';
   var hex = '#';
-  for (var i = 0; i < 6; i++) {
-    hex += digits[Math.floor(Math.random() * 16)];
-  }
+    for (var i = 0; i < 6; i++) {
+      hex += digits[Math.floor(Math.random() * 16)];
+    }
   return hex;
 }
 
 function displayColors() {
   palettes.forEach(palette => {
-    let hex = generateHex();
-    palette.css('background-color', hex)
-    palette.find('.hexcode').text(hex);
+    var hex;
+    if (!palette.children('img').hasClass('locked')) {
+      hex = generateHex();
+      palette.css('background-color', hex)
+      palette.find('.hexcode').text(hex); 
+    }
+    savePalette(hex);
   });
-  lockPalette();
 }
 
-function toggleImage() {
+function toggleImage() { 
   var locked = './images/locked.svg';
   var unlocked = './images/unlocked.svg';
-  var src = $(this).attr('src') === locked ? unlocked : locked;
+  var src = $(this).attr('src') === unlocked ? locked : unlocked;
 
+  $(this).toggleClass('locked');
   $(this).attr('src', src);
 }
 
-// function lockPalette() {
-//   if($('.palettes').find('.locker').hasClass('locked')) {
-//     $('.palettes').find('.hexcode') === 
-//   } else {
+function saveProject(e) {
+  e.preventDefault();
+  var projectName = $('.project-name').val();
 
-//   }
-// }
+  $('.project-list').prepend(`
+    <option>${projectName}</option>
+  `);
+
+  $('.project-name').val('');
+};
+
+function savePalette(e) {
+  e.preventDefault();
+  var paletteName = $('.palette-name').val();
+
+  $('.all-projects').prepend(`
+    <div>
+      
+    </div>
+  `)  
+}
--- server.js
+++ server.js
@@ -1,17 +1,75 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // import express library via NPM install
+const app = express(); // instantiate express
+const bodyParser = require('body-parser'); // import bodyparser module
 
-app.use(express.static('public'));
+app.set('port', process.env.PORT || 3000); // set port variable with default at 3000
+app.locals.title = 'Palette Picker'; // 
+app.locals.palettes  = [];
+app.locals.projects = [];
 
-app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Palette Picker';
+app.use(bodyParser.json()); // use bodyParser in app
+// app.use(bodyParser.urlencoded({ extended: true });
+app.use(express.static('public')); // 
+
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get('/', (request, response) => {
   response.send('Palette Serverrr!')
 });
 
-// app.get('/api/v1/palettePicker', (request, response) => {
-//   response.status(200).json();
+app.get('/api/v1/palettes', (request, response) => {
+  const palettes = app.locals.palettes;
+
+  if (palettes) {
+    response.status(200).json({ palettes });   
+  } else {
+    return response.status(404).json('Page Not Found');
+  }
+});
+
+app.get('/api/v1/projects', (request, response) => {
+  const projects = app.locals.projects;
+
+  if(projects) {
+    return response.status(200).json({ projects });
+  } else {
+    return response.status(404).json('Page Not Found');
+  }
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+  const id = Date.now();
+  const { palettes } = request.body;
+
+  if(!palettes) {
+    return response.status(422).send({ error: 'Well-formed request but containing semantic errors' });
+  } else {
+    app.locals.palettes.push({ id, palettes });
+    return response.status(201).json({ id, palettes });
+  }
+});
+
+app.post('/api/v1/projects', (request, response) => {
+  const id = Date.now();
+  const { projects } = request.body;
+
+  if(!projects) {
+    return response.status(422).send({ error: 'Well-formed request but containing semantic errors '});
+  } else {
+    app.locals.projects.push({ id, projects });
+    return  repponse.status(201).json({ id, projects });
+  }
+});
+
+// app.delete('api/v1/palettes:id', (request, response) => {
+//  
+// });
+
+// app.delete('api/v1/projects:id', (request, response) => {
+//  
 // });
 
 app.listen(app.get('port'), () => {
