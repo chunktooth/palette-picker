d2950a66dc446c407a16690a62c4cd8293d119c8
chunktooth <om.saetehn@gmail.com>
2018-05-04 11:00:47 -0600

    added config

--- knexfile.js
+++ knexfile.js
@@ -11,20 +11,25 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/ppicker',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
   }
-  // production: {
-    // client: 'postgresql',
-    // connection: {
-    //   database: 'my_db',
-    //   user:     'username',
-    //   password: 'password'
-    // },
-    // pool: {
-    //   min: 2,
-    //   max: 10
-    // },
-    // migrations: {
-    //   tableName: 'knex_migrations'
-    // }
-  // }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
+
 };
